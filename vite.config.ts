998ae306f0a5564f649428812a57e2982b2e517e
UladzislauPapliouka <uladzislau.papliouka@gmail.com>
2023-11-12 22:05:11 +0300

    feat(config): add aliases

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,13 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-
-// https://vitejs.dev/config/
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import * as path from "path";
 export default defineConfig({
   plugins: [react()],
-})
+  resolve: {
+    // adding aliases
+    alias: {
+      "@root": path.resolve(__dirname, "./src"),
+      "@assets": path.resolve(__dirname, "./src/assets"),
+    },
+  },
+});
